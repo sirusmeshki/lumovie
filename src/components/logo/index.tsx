1851c1542008f39edfe59ee85742aefa9bae62e7
AmirHossein <sirusmeshki@gmail.com>
2024-08-27 15:28:14 +0330

    fix: Logo warning, nav width on mobile

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,28 +1,30 @@
-import { FC } from "react";
+import { FC } from 'react'
 
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Logo: FC = () => {
-  return (
-    <Link href="/" className="flex min-w-fit">
-      <Image
-        src="/assets/icon/Logo_Icon.svg"
-        alt="logo"
-        width={42}
-        height={42}
-        priority
-      />
-      <Image
-        src="/assets/icon/Logo_Text.svg"
-        alt="logo"
-        width={108}
-        height={34}
-        className="hidden sm:block"
-        priority
-      />
-    </Link>
-  );
-};
+    return (
+        <Link
+            href='/'
+            className='flex min-w-fit'>
+            <Image
+                src='/assets/icon/Logo_Icon.svg'
+                alt='logo'
+                width={42}
+                height={42}
+                priority
+            />
+            <Image
+                src='/assets/icon/Logo_Text.svg'
+                alt='logo'
+                width={108}
+                height={34}
+                className='hidden sm:block w-auto h-auto'
+                priority
+            />
+        </Link>
+    )
+}
 
-export default Logo;
+export default Logo
--- src/layout/header/nav/index.tsx
+++ src/layout/header/nav/index.tsx
@@ -1,23 +1,23 @@
-import Link from "next/link";
-import { FC } from "react";
+import Link from 'next/link'
+import { FC } from 'react'
 
 const Nav: FC = () => {
-  const links = ["pricing", "download", "faq"];
+    const links = ['pricing', 'download', 'faq']
 
-  return (
-    <nav className="flex justify-between gap-4 max-w-96 sm:w-96">
-      {links.map((link, index) => {
-        return (
-          <Link
-            key={index}
-            className="text-xs font-medium text-white capitalize md:text-sm xl:text-base last:uppercase"
-            href={`/${link}`}>
-            {link}
-          </Link>
-        );
-      })}
-    </nav>
-  );
-};
+    return (
+        <nav className='flex justify-between w-full gap-4 sm:max-w-80 sm:w-80'>
+            {links.map((link, index) => {
+                return (
+                    <Link
+                        key={index}
+                        className='text-xs font-medium text-white capitalize md:text-sm xl:text-base last:uppercase'
+                        href={`/${link}`}>
+                        {link}
+                    </Link>
+                )
+            })}
+        </nav>
+    )
+}
 
-export default Nav;
+export default Nav
